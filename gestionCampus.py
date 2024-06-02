94e74c75d3575d107a3923ac1a4f5d231b88f6d9
Laura <8.10carrillolaura@gmail.com>
2024-06-02 15:43:18 -0500

    registro de notas para camper nuevo

--- gestionCampus.py
+++ gestionCampus.py
@@ -1,5 +1,7 @@
 import datos
 
+#Funciones Coordinador
+#Nuevo Camper
 def registrarCamper():
     camper = {
         "cc": input("Ingrese el numero de documento del nuevo camper: "),
@@ -17,3 +19,34 @@ def registrarCamper():
     datos.data["campers"].append(camper)
     datos.guardarDatos()
     print("Camper registrado con exito")
+
+#Registrar nota de camper registrado
+def registrarNotaCamper():
+    cc = input("Ingrese el numero de documento del camper: ")
+
+    for camper in datos.data["campers"]:
+        if camper["cc"] == cc:
+            print(f"Camper encontrado: {camper["nombre"]}{camper["apellido"]}")
+
+        notaRegistroTeo = int(input("Ingrese la nota teorica del camper: "))
+        notaRegistroPrac = int(input("Ingrese la nota practica del camper: "))
+
+        promedio = (notaRegistroTeo + notaRegistroPrac)//2
+
+        if promedio >= 60:
+            camper["estatus"] = "Aprobado"
+            camper["riesgo"] = "bajo"
+        else:
+            camper["estatus"] = "Reprobado"
+            camper["riesgo"] = "alto"
+        
+        camper["notasRegistro"] = {
+            "notaTeorica": notaRegistroTeo,
+            "notaPractica": notaRegistroPrac,
+            "promedio": promedio
+        }
+
+        datos.guardarDatos()
+        print("Notas registradas y camper actualizado con exito")
+        return
+    print("No se encontro el camper")
--- menu.py
+++ menu.py
@@ -4,8 +4,25 @@ import gestionCampus
 def menuPrincipal():
     datos.cargarDatos()
     while True:
-        print("Bienvenido al sistema de gestion de Campus")
-        print("1. Registrar nuevo Camper")
+        print("Bienvenido al sistema de gestion de CampusLands")
+        print("1. Coordinador")
+        print("2. Trainer")
+        print("3. Camper")
+        print("4. Salir")
+        opc = 0
+        try:
+            opc = int(input("Ingrese la opcion deseada: "))
+        except Exception:
+            print("Opcion invalida")
+        if opc == 1:
+            menuCoordinador()
+    
+
+def menuCoordinador():
+    while True:
+        print("Bienvenido Coordinador")
+        print("1. Registrar camper")
+        print("2. Registrar nota de camper registrado")
         opc = 0
         try:
             opc = int(input("Ingrese la opcion deseada: "))
@@ -13,3 +30,6 @@ def menuPrincipal():
             print("Opcion invalida")
         if opc == 1:
             gestionCampus.registrarCamper()
+        elif opc == 2:
+            gestionCampus.registrarNotaCamper()
+        
