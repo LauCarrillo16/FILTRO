8b0c3390ff22989455b1f208f91d400f09208cf6
LauCarrillo16 <8.10carrillolaura@gmail.com>
2024-06-05 08:53:23 -0500

    menu

--- menu.py
+++ menu.py
@@ -16,10 +16,13 @@ def menuPrincipal():
             print("Opcion invalida")
         if opc == 1:
             menuCoordinador()
-        if opc == 2:
+        elif opc == 2:
             menuTrainer()
-        if opc == 3:
+        elif opc == 3:
             menuCamper()
+        elif opc == 4:
+            break
+        
     
 
 def menuCoordinador():
@@ -33,6 +36,7 @@ def menuCoordinador():
         print("5. Registrar Trainer")
         print("6. Matriculas (asignar camper a ruta o trainer a ruta)")
         print("7. Reportes")
+        print("8. Regresar al menu principal")
 
         opc = 0
         try:
@@ -53,6 +57,8 @@ def menuCoordinador():
             menuMatriculas()
         elif opc == 7:
             menuReportes()
+        elif opc == 8:
+            break
 
         
 def menuMatriculas():
@@ -61,6 +67,7 @@ def menuMatriculas():
         print("Gestion de Matriculas")
         print("1. Asignar ruta a un Camper")
         print("2. Asignar Trainer a ruta y stack")
+        print("3. Regresar al menu anterior")
         opc = 0
         try:
             opc = int(input("Ingrese la opcion deseada: "))
@@ -70,6 +77,8 @@ def menuMatriculas():
             gestionCampus.asignarRuta()
         elif opc == 2:
             gestionCampus.asginarTrainerRuta()
+        elif opc == 3:
+            break
 
 def menuReportes():
     datos.cargarDatos()
@@ -81,6 +90,7 @@ def menuReportes():
         print("4. Trainers de CampusLands")
         print("5. Campers y Trainers asociados a una ruta")
         print("6. Cuantos Campers perdieron y aprobaron los modulos y su entrenador")
+        print("7. Regresar al menu de Coordinador")
         opc = 0
         try:
             opc = int(input("Ingrese la opcion deseada: "))
@@ -98,6 +108,8 @@ def menuReportes():
             gestionCampus.campersYtrainersRuta()
         elif opc == 6:
             gestionCampus.cuantosPYA()
+        elif opc == 7:
+            break
 
             
 def menuTrainer():
@@ -105,6 +117,7 @@ def menuTrainer():
     while True:
         print("Bienvenido Trainer")
         print("1. Asignar notas del modulo")
+        print("2. Regresar al menu principal")
         opc = 0
         try:
             opc = int(input("Ingrese la opcion deseada: "))
@@ -112,12 +125,15 @@ def menuTrainer():
             print("Opcion invalida")
         if opc == 1:
             gestionCampus.evaluarModulo()
+        elif opc == 2:
+            break
 
 def menuCamper():
     datos.cargarDatos()
     while True:
         print("Bienvenido Camper")
         print("1. Ver mis datos")
+        print("2. Regresar al menu principal")
         opc = 0
         try:
             opc = int(input("Ingrese la opcion deseada: "))
@@ -125,3 +141,5 @@ def menuCamper():
             print("Opcion invalida")
         if opc == 1:
             gestionCampus.verMisDatos()
+        elif opc == 2:
+            break
